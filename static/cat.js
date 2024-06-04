07c61d693aafbce694c5cbb5345734f53824c32b
mumsuz <mumsuz@outlook.com>
2024-06-04 15:49:20 +0300

    update

--- static/cat.js
+++ static/cat.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('catForm');
-    form.addEventListener('submit', function calculateCat(event) {
+    form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const q1 = parseInt(document.getElementById('q1').value);
