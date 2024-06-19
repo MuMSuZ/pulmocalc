af28ef0c8fbd178f15ccdf1e515892ce0f48367a
MuMSuZ <mumsuz@outlook.com>
2024-06-19 23:15:16 +0300

    update

---
+++ oah/bronsit/test.js
@@ -0,0 +1,16 @@
+function togglePanel(panelId) {
+    const panel = document.getElementById(panelId);
+    if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+    } else {
+        panel.style.display = 'block';
+    }
+}
+
+window.onload = function() {
+    // Tüm panellerin gizli olmasını sağla
+    const panels = document.querySelectorAll('.panel, .alt-panel');
+    panels.forEach(panel => {
+        panel.style.display = 'none';
+    });
+};
