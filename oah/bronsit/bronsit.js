2adc39fc8e069b8b651ea9bdb687bc5397cbd686
MuMSuZ <mumsuz@outlook.com>
2024-06-19 22:54:26 +0300

    update

--- oah/bronsit/bronsit.js
+++ oah/bronsit/bronsit.js
@@ -1,3 +1,4 @@
+    // akordiyon 
 document.addEventListener('DOMContentLoaded', function() {
     const acc = document.getElementsByClassName('accordion');
     
@@ -13,3 +14,43 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+    // .txt dosya tanımlama
+    const dosyaListesi = [
+        { dosyaYolu: 'koahnedir.txt', hedefId: 'koahnedir' },
+        { dosyaYolu: 'data/dosya2.txt', hedefId: 'metin-alani-2' },
+        { dosyaYolu: 'data/dosya3.txt', hedefId: 'metin-alani-3' }
+    ];
+    
+    function txtDosyasiniYukle(dosyaYolu, hedefId) {
+        fetch(dosyaYolu)
+            .then(response => response.text())
+            .then(data => {
+                const metinAlani = document.getElementById(hedefId);
+                const lines = data.split('\n');
+                lines.forEach(line => {
+                    let formattedLine = line
+                        .replace(/<bold>(.*?)<\/bold>/g, '<span class="bold">$1</span>')
+                        .replace(/<italic>(.*?)<\/italic>/g, '<span class="italic">$1</span>')
+                        .replace(/<indent>(.*?)<\/indent>/g, '<span class="indent">$1</span>')
+                        .replace(/<margin-top>(.*?)<\/margin-top>/g, '<div class="margin-top">$1</div>')
+                        .replace(/<margin-bottom>(.*?)<\/margin-bottom>/g, '<div class="margin-bottom">$1</div>')
+                        .replace(/<red>(.*?)<\/red>/g, '<span class="color-red">$1</span>')
+                        .replace(/<blue>(.*?)<\/blue>/g, '<span class="color-blue">$1</span>')
+                        .replace(/<green>(.*?)<\/green>/g, '<span class="color-green">$1</span>')
+                        .replace(/<yellow>(.*?)<\/yellow>/g, '<span class="color-yellow">$1</span>')
+                        .replace(/<purple>(.*?)<\/purple>/g, '<span class="color-purple">$1</span>');
+                    metinAlani.innerHTML += formattedLine + '<br>';
+                });
+            })
+            .catch(error => console.error('Hata:', error));
+    }
+    
+    function tumDosyalariYukle() {
+        dosyaListesi.forEach(dosya => {
+            txtDosyasiniYukle(dosya.dosyaYolu, dosya.hedefId);
+        });
+    }
+    
+    window.onload = tumDosyalariYukle;
+    
