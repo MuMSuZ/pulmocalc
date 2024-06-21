5c0832820d778c200b7fdc691afd8255ad224fe2
MuMSuZ <mumsuz@outlook.com>
2024-06-21 17:56:23 +0300

    update

--- oah/koah/koah.js
+++ oah/koah/koah.js
@@ -60,3 +60,25 @@
     
     window.onload = tumDosyalariYukle;
     
+
+    // Açılır Pencere Modülü
+
+    var modal = document.getElementById("myModal");
+        var btns = document.getElementsByClassName("openModal");
+        var span = document.getElementsByClassName("close")[0];
+
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].onclick = function() {
+                modal.style.display = "block";
+            }
+        }
+
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
