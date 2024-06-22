c3269dacf3350ce284d0d2855548b74457e01f9b
MuMSuZ <mumsuz@outlook.com>
2024-06-22 21:21:21 +0300

    update

--- oah/bronsit/test.js
+++ oah/bronsit/test.js
@@ -1,21 +1,3 @@
-function togglePanel(panelId) {
-    const panel = document.getElementById(panelId);
-    if (panel.style.display === 'block') {
-        panel.style.display = 'none';
-    } else {
-        panel.style.display = 'block';
-    }
-}
-            
-
-window.onload = function() {
-    // Tüm panellerin gizli olmasını sağla
-    const panels = document.querySelectorAll('.panel, .alt-panel');
-    panels.forEach(panel => {
-        panel.style.display = 'none';
-    });
-};
-
 // .txt dosyasını okuma
 fetch('metin/konu.txt')
 .then(response => response.text())
@@ -53,6 +35,23 @@ fetch('metin/konu.txt')
             document.getElementById('myModal').style.display = 'block';
         });
     });
+    function togglePanel(panelId) {
+        const panel = document.getElementById(panelId);
+        if (panel.style.display === 'block') {
+            panel.style.display = 'none';
+        } else {
+            panel.style.display = 'block';
+        }
+    }
+                
+    
+    window.onload = function() {
+        // Tüm panellerin gizli olmasını sağla
+        const panels = document.querySelectorAll('.panel, .alt-panel');
+        panels.forEach(panel => {
+            panel.style.display = 'none';
+        });
+    };
 })
 .catch(error => console.error('Error:', error));
 
