8f5d87a0962996fafc3fe0b9f554af10f9c81919
MuMSuZ <mumsuz@outlook.com>
2024-06-23 00:36:04 +0300

    update

--- oah/koah/eski.js
+++ oah/koah/eski.js
@@ -28,21 +28,6 @@ function txtDosyasiniYukle(dosyaYolu, hedefId) {
         .then(data => {
             const metinAlani = document.getElementById(hedefId);
             const lines = data.split('\n');
-
-                // Anahtar kelimeler ve ilgili açıklamalar
-            const keywordData = {
-                'Şekil 1.1': {
-                    description: 'Şekil 1.1',
-                    image: 'resim/sekil11.png'
-                },
-                'Fletcher': {
-                    description: 'ne olduğunu yaz'
-                },
-                'Peto': {
-                    description: 'ne olduğunu yaz'
-                }
-                // Diğer kelimeler ve açıklamaları buraya ekleyebilirsiniz
-            };
             lines.forEach(line => {
                 let formattedLine = line
                     .replace(/<bold>(.*?)<\/bold>/g, '<span class="bold">$1</span>')
@@ -51,41 +36,15 @@ function txtDosyasiniYukle(dosyaYolu, hedefId) {
                     .replace(/<indented-bolum>(.*?)<\/indented-bolum>/g, '<span class="indented-bolum">$1</span>')
                     .replace(/<margin-top>(.*?)<\/margin-top>/g, '<div class="margin-top">$1</div>')
                     .replace(/<margin-bottom>(.*?)<\/margin-bottom>/g, '<div class="margin-bottom">$1</div>')
+                    .replace(/<red>(.*?)<\/red>/g, '<span class="red">$1</span>')
+                    .replace(/<blue>(.*?)<\/blue>/g, '<span class="blue">$1</span>')
+                    .replace(/<green>(.*?)<\/green>/g, '<span class="green">$1</span>')
+                    .replace(/<yellow>(.*?)<\/yellow>/g, '<span class="yellow">$1</span>')
+                    .replace(/<purple>(.*?)<\/purple>/g, '<span class="purple">$1</span>')
                     .replace(/<chamois>(.*?)<\/chamois>/g, '<span class="chamois">$1</span>')
                     .replace(/<redorange>(.*?)<\/redorange>/g, '<span class="redorange">$1</span>')
                     .replace(/<coralblue>(.*?)<\/coralblue>/g, '<span class="coralblue">$1</span>');
-                
-                    // Her anahtar kelime için metni kontrol et ve etiket ekle
-                for (let [keyword, data] of Object.entries(keywordData)) {
-                    let regex = new RegExp(`${keyword}`, 'gu');
-                    formattedLine = formattedLine.replace(regex, `<a href="#" class="keyword" data-description="${data.description}" data-image="${data.image}">${keyword}</a>`);
-                }
-
-                    metinAlani.innerHTML += formattedLine + '<br>';
-            });
-
-            // Tıklama olaylarını ayarla
-            document.querySelectorAll('.keyword').forEach(element => {
-                element.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    const description = this.getAttribute('data-description');
-                    const image = this.getAttribute('data-image');
-                    document.getElementById('modal-text').textContent = description;
-                    document.getElementById('modal-image').src = image;
-                    document.getElementById('myModal').style.display = "block";
-                });
-            });
-
-            // Modal kapatma düğmesi
-            document.querySelector('.close').addEventListener('click', function() {
-                document.getElementById('myModal').style.display = "none";
-            });
-
-            // Modal dışına tıklanırsa kapatma
-            window.addEventListener('click', function(event) {
-                if (event.target == document.getElementById('myModal')) {
-                    document.getElementById('myModal').style.display = "none";
-                }
+                metinAlani.innerHTML += formattedLine + '<br>';
             });
         })
         .catch(error => console.error('Hata:', error));
