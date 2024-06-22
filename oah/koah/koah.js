27a1de73fe9f1eb6d1abc6583e945f3499895900
MuMSuZ <mumsuz@outlook.com>
2024-06-22 22:45:59 +0300

    update

--- oah/koah/koah.js
+++ oah/koah/koah.js
@@ -44,6 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         .replace(/<chamois>(.*?)<\/chamois>/g, '<span class="chamois">$1</span>')
                         .replace(/<redorange>(.*?)<\/redorange>/g, '<span class="redorange">$1</span>')
                         .replace(/<coralblue>(.*?)<\/coralblue>/g, '<span class="coralblue">$1</span>')
+                        .replace(/\bŞekil\b/g, '<a href="mumsuz.github.oi/pulmocalc">Şekil 1.1</a>');
                     metinAlani.innerHTML += formattedLine + '<br>';
                 });
             })
