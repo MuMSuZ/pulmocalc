54b0a3aaff72906efa1bf9ebcc194d669f706bc6
MuMSuZ <mumsuz@outlook.com>
2024-06-26 16:49:16 +0300

    update

--- oah/koah/yeni.js
+++ oah/koah/yeni.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { dosyaYolu: 'metin/koahnedir.txt', hedefId: 'koahnedir' },
         { dosyaYolu: 'metin/koahyuku.txt', hedefId: 'koahyuku' },
         { dosyaYolu: 'metin/patogenez.txt', hedefId: 'patogenez' },
-        { dosyaYolu: 'metin/supheindeksi.txt', descriptionFile: 'supheindeksi' }
+        { dosyaYolu: 'metin/supheindeksi.txt', hedefId: 'supheindeksi' }
     ];
 
     const keywordData = {
@@ -43,7 +43,20 @@ document.addEventListener('DOMContentLoaded', function() {
         for (let keyword in keywordData) {
             if (keywordData[keyword].descriptionFile) {
                 const response = await fetch(keywordData[keyword].descriptionFile);
-                const data = await response.text();
+                let data = await response.text();
+                
+                // Biçimlendirme işlemleri
+                data = data
+                    .replace(/<bold>(.*?)<\/bold>/g, '<span class="bold">$1</span>')
+                    .replace(/<italic>(.*?)<\/italic>/g, '<span class="italic">$1</span>')
+                    .replace(/<indent>(.*?)<\/indent>/g, '<span class="indent">$1</span>')
+                    .replace(/<indented-bolum>(.*?)<\/indented-bolum>/g, '<span class="indented-bolum">$1</span>')
+                    .replace(/<margin-top>(.*?)<\/margin-top>/g, '<div class="margin-top">$1</div>')
+                    .replace(/<margin-bottom>(.*?)<\/margin-bottom>/g, '<div class="margin-bottom">$1</div>')
+                    .replace(/<chamois>(.*?)<\/chamois>/g, '<span class="chamois">$1</span>')
+                    .replace(/<redorange>(.*?)<\/redorange>/g, '<span class="redorange">$1</span>')
+                    .replace(/<coralblue>(.*?)<\/coralblue>/g, '<span class="coralblue">$1</span>');
+                
                 keywordData[keyword].description = data;
             }
         }
