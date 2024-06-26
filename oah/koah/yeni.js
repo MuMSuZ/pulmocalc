5d946b30d0482788a1b600a451da2b2b88f33837
MuMSuZ <mumsuz@outlook.com>
2024-06-26 17:57:13 +0300

    update

--- oah/koah/yeni.js
+++ oah/koah/yeni.js
@@ -15,10 +15,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // .txt dosya tanımlama
     const dosyaListesi = [
-        { dosyaYolu: 'metin/koahnedir.txt', hedefId: 'koahnedir' },
-        { dosyaYolu: 'metin/koahyuku.txt', hedefId: 'koahyuku' },
-        { dosyaYolu: 'metin/patogenez.txt', hedefId: 'patogenez' },
-        { dosyaYolu: 'metin/supheindeksi.txt', hedefId: 'supheindeksi' }
+        { dosyaYolu: 'metin/koahnedir.txt', hedefId: 'koahnedir', modal: false },
+        { dosyaYolu: 'metin/koahyuku.txt', hedefId: 'koahyuku', modal: false },
+        { dosyaYolu: 'metin/patogenez.txt', hedefId: 'patogenez', modal: false },
+        { dosyaYolu: 'metin/supheindeksi.txt', modal: true } // modal: true olarak belirttik
     ];
 
     const keywordData = {
@@ -62,65 +62,69 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    async function txtDosyasiniYukle(dosyaYolu, hedefId) {
+    async function txtDosyasiniYukle(dosyaYolu, hedefId, modal) {
         try {
             const response = await fetch(dosyaYolu);
             const data = await response.text();
-            const metinAlani = document.getElementById(hedefId);
-            const lines = data.split('\n');
+            if (modal) {
+                // Modal için olan metinleri keywordData'ya ekle
+                keywordData[dosyaYolu] = { description: data };
+            } else {
+                const metinAlani = document.getElementById(hedefId);
+                const lines = data.split('\n');
 
-            await yukleKeywordData();
+                await yukleKeywordData();
 
-            lines.forEach(line => {
-                let formattedLine = line
-                    .replace(/<bold>(.*?)<\/bold>/g, '<span class="bold">$1</span>')
-                    .replace(/<italic>(.*?)<\/italic>/g, '<span class="italic">$1</span>')
-                    .replace(/<indent>(.*?)<\/indent>/g, '<span class="indent">$1</span>')
-                    .replace(/<indented-bolum>(.*?)<\/indented-bolum>/g, '<span class="indented-bolum">$1</span>')
-                    .replace(/<margin-top>(.*?)<\/margin-top>/g, '<div class="margin-top">$1</div>')
-                    .replace(/<margin-bottom>(.*?)<\/margin-bottom>/g, '<div class="margin-bottom">$1</div>')
-                    .replace(/<chamois>(.*?)<\/chamois>/g, '<span class="chamois">$1</span>')
-                    .replace(/<redorange>(.*?)<\/redorange>/g, '<span class="redorange">$1</span>')
-                    .replace(/<coralblue>(.*?)<\/coralblue>/g, '<span class="coralblue">$1</span>');
-                
-                // Her anahtar kelime için metni kontrol et ve etiket ekle
-                for (let [keyword, data] of Object.entries(keywordData)) {
-                    let regex = new RegExp(`${keyword}`, 'gu');
-                    formattedLine = formattedLine.replace(regex, `<a href="#" class="keyword" data-description="${data.description}" data-image="${data.image || ''}">${keyword}</a>`);
-                }
-
-                metinAlani.innerHTML += formattedLine + '<br>';
-            });
-
-            // Tıklama olaylarını ayarla
-            document.querySelectorAll('.keyword').forEach(element => {
-                element.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    const description = this.getAttribute('data-description');
-                    const image = this.getAttribute('data-image');
-                    document.getElementById('modal-text').innerHTML = description; // innerHTML kullanarak biçimlendirmeleri uygular
-                    if (image) {
-                        document.getElementById('modal-image').src = image;
-                        document.getElementById('modal-image').style.display = 'block';
-                    } else {
-                        document.getElementById('modal-image').style.display = 'none';
+                lines.forEach(line => {
+                    let formattedLine = line
+                        .replace(/<bold>(.*?)<\/bold>/g, '<span class="bold">$1</span>')
+                        .replace(/<italic>(.*?)<\/italic>/g, '<span class="italic">$1</span>')
+                        .replace(/<indent>(.*?)<\/indent>/g, '<span class="indent">$1</span>')
+                        .replace(/<indented-bolum>(.*?)<\/indented-bolum>/g, '<span class="indented-bolum">$1</span>')
+                        .replace(/<margin-top>(.*?)<\/margin-top>/g, '<div class="margin-top">$1</div>')
+                        .replace(/<margin-bottom>(.*?)<\/margin-bottom>/g, '<div class="margin-bottom">$1</div>')
+                        .replace(/<chamois>(.*?)<\/chamois>/g, '<span class="chamois">$1</span>')
+                        .replace(/<redorange>(.*?)<\/redorange>/g, '<span class="redorange">$1</span>')
+                        .replace(/<coralblue>(.*?)<\/coralblue>/g, '<span class="coralblue">$1</span>');
+                    
+                    // Her anahtar kelime için metni kontrol et ve etiket ekle
+                    for (let [keyword, data] of Object.entries(keywordData)) {
+                        let regex = new RegExp(`${keyword}`, 'gu');
+                        formattedLine = formattedLine.replace(regex, `<a href="#" class="keyword" data-description="${data.description}" data-image="${data.image || ''}">${keyword}</a>`);
                     }
-                    document.getElementById('myModal').style.display = "block";
+
+                    metinAlani.innerHTML += formattedLine + '<br>';
                 });
-            });
 
-            // Modal kapatma düğmesi
-            document.querySelector('.close').addEventListener('click', function() {
-                document.getElementById('myModal').style.display = "none";
-            });
+                // Tıklama olaylarını ayarla
+                document.querySelectorAll('.keyword').forEach(element => {
+                    element.addEventListener('click', function(event) {
+                        event.preventDefault();
+                        const description = this.getAttribute('data-description');
+                        const image = this.getAttribute('data-image');
+                        document.getElementById('modal-text').innerHTML = description; // innerHTML kullanarak biçimlendirmeleri uygular
+                        if (image) {
+                            document.getElementById('modal-image').src = image;
+                            document.getElementById('modal-image').style.display = 'block';
+                        } else {
+                            document.getElementById('modal-image').style.display = 'none';
+                        }
+                        document.getElementById('myModal').style.display = "block";
+                    });
+                });
 
-            // Modal dışına tıklanırsa kapatma
-            window.addEventListener('click', function(event) {
-                if (event.target == document.getElementById('myModal')) {
+                // Modal kapatma düğmesi
+                document.querySelector('.close').addEventListener('click', function() {
                     document.getElementById('myModal').style.display = "none";
-                }
-            });
+                });
 
+                // Modal dışına tıklanırsa kapatma
+                window.addEventListener('click', function(event) {
+                    if (event.target == document.getElementById('myModal')) {
+                        document.getElementById('myModal').style.display = "none";
+                    }
+                });
+            }
         } catch (error) {
             console.error('Hata:', error);
         }
@@ -128,7 +132,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function tumDosyalariYukle() {
         dosyaListesi.forEach(dosya => {
-            txtDosyasiniYukle(dosya.dosyaYolu, dosya.hedefId);
+            txtDosyasiniYukle(dosya.dosyaYolu, dosya.hedefId, dosya.modal);
         });
     }
 
