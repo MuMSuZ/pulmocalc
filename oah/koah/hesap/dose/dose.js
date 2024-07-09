c1c90cba382f78cf13ac858b9e2ada017296836e
MuMSuZ <mumsuz@outlook.com>
2024-07-09 16:48:47 +0300

    update

---
+++ oah/koah/hesap/dose/dose.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('doseForm');
+    const resultContainer = document.querySelector('.result');
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const dyspnea = parseInt(document.getElementById('dyspnea').value);
+        const obstruction = parseInt(document.getElementById('obstruction').value);
+        const smokingStatus = parseInt(document.getElementById('smoking_status').value);
+        const exacerbations = parseInt(document.getElementById('exacerbations').value);
+
+        const doseScore = dyspnea + obstruction + smokingStatus + exacerbations;
+
+        let comment = '';
+        if (doseScore <= 3) {
+            comment = 'Düşük alevlenme riski';
+        } else {
+            comment = 'Yüksek alevlenme riski';
+        }
+
+        resultContainer.innerHTML = `Sonuç: ${doseScore} <br><br> Yorum: ${comment}`;
+    });
+
+    resultContainer.innerHTML = '';
+});
